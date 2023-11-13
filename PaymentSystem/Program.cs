9444eb73997b38b7e9b1a7d9fe626cd25420923c
Marcio Del Bianco <marcio.bianco@gmail.com>
2023-11-12 23:31:47 -0300

    remove unused DI

--- PaymentSystem/Program.cs
+++ PaymentSystem/Program.cs
@@ -10,7 +10,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IPaymentOption, CreditCard>();
-builder.Services.AddScoped<IPaymentOption, Cash>();
 
 var app = builder.Build();
 
